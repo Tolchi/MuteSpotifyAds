af75de4e929936c755d42f78751a4558eb756e5d
Se Hoon Park <sehoon.park@gmail.com>
2026-04-28 00:05:32 -0300

    Initial Commit

---
+++ MuteSpotifyAds/MuteSpotifyAdsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MuteSpotifyAdsApp.swift
+//  MuteSpotifyAds
+//
+//  Created by Se Hoon Park on 28/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct MuteSpotifyAdsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MuteSpotifyAdsTests/MuteSpotifyAdsTests.swift
@@ -0,0 +1,19 @@
+//
+//  MuteSpotifyAdsTests.swift
+//  MuteSpotifyAdsTests
+//
+//  Created by Se Hoon Park on 28/04/2026.
+//
+
+import Testing
+@testable import MuteSpotifyAds
+
+struct MuteSpotifyAdsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
